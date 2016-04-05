3ff60c44743a578076ade71ec5e926bd6f40a1bc
jbytw <JBY@JBYTW-MBP.local>
2016-04-05 09:03:52 +0800

    print out-of-bag accuracy

---
+++ oob_plotter.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+
+from collections import OrderedDict
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from numpy import genfromtxt, savetxt
+from os import path
+
+RANDOM_STATE = 123
+
+# create the training & test sets, skipping the header row with [1:]
+script_dir = path.dirname(__file__)  # <-- absolute dir the script is in
+dataset = genfromtxt(open(path.join(script_dir, 'data/train.csv'), 'r'),
+                        delimiter=',', dtype='f8')[1:]
+target = [x[0] for x in dataset]
+train = [x[1:] for x in dataset]
+test = genfromtxt(open(path.join(script_dir, 'data/test.csv'),
+                        'r'), delimiter=',', dtype='f8')[1:]
+
+
+# NOTE: Setting the `warm_start` construction parameter to `True` disables
+# support for paralellised ensembles but is necessary for tracking the OOB
+# error trajectory during training.
+ensemble_clfs = [
+    ("RandomForestClassifier, max_features='sqrt'",
+        RandomForestClassifier(warm_start=True, oob_score=True,
+                               max_features="sqrt",
+                               random_state=RANDOM_STATE)),
+    ("RandomForestClassifier, max_features='log2'",
+        RandomForestClassifier(warm_start=True, max_features='log2',
+                               oob_score=True,
+                               random_state=RANDOM_STATE)),
+    ("RandomForestClassifier, max_features=None",
+        RandomForestClassifier(warm_start=True, max_features=None,
+                               oob_score=True,
+                               random_state=RANDOM_STATE))
+]
+
+# Map a classifier name to a list of (<n_estimators>, <error rate>) pairs.
+error_rate = OrderedDict((label, []) for label, _ in ensemble_clfs)
+
+# Range of `n_estimators` values to explore.
+min_estimators = 20
+max_estimators = 200
+
+for label, clf in ensemble_clfs:
+    for i in range(min_estimators, max_estimators + 1):
+        clf.set_params(n_estimators=i)
+        clf.fit(train, target)
+
+        # Record the OOB error for each `n_estimators=i` setting.
+        oob_error = 1 - clf.oob_score_
+        error_rate[label].append((i, oob_error))
+
+# Generate the "OOB error rate" vs. "n_estimators" plot.
+for label, clf_err in error_rate.items():
+    xs, ys = zip(*clf_err)
+    plt.plot(xs, ys, label=label)
+
+plt.xlim(min_estimators, max_estimators)
+plt.xlabel("n_estimators")
+plt.ylabel("OOB error rate")
+plt.legend(loc="upper right")
+plt.show()
--- recognizer.py
+++ recognizer.py
@@ -1,25 +1,27 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import log_loss
 from numpy import genfromtxt, savetxt
 from os import path
 import matplotlib.pyplot as plt
 
-def main():
-    script_dir = path.dirname(__file__) #<-- absolute dir the script is in
+# create the training & test sets, skipping the header row with [1:]
+script_dir = path.dirname(__file__)  # <-- absolute dir the script is in
+dataset = genfromtxt(open(path.join(script_dir, 'data/train.csv'), 'r'),
+                        delimiter=',', dtype='f8')[1:]
+target = [x[0] for x in dataset]
+train = [x[1:] for x in dataset]
+test = genfromtxt(open(path.join(script_dir, 'data/test.csv'),
+                        'r'), delimiter=',', dtype='f8')[1:]
 
-    # create the training & test sets, skipping the header row with [1:]
-    dataset = genfromtxt(open(path.join(script_dir, 'data/train.csv'), 'r'),
-                             delimiter=',', dtype='f8')[1:]
-    target = [x[0] for x in dataset]
-    train = [x[1:] for x in dataset]
-    test = genfromtxt(open(path.join(script_dir, 'data/test.csv'), 'r'), delimiter=',', dtype='f8')[1:]
+# create and train the random forest
+# multi-core CPUs can use: rf =
+# RandomForestClassifier(n_estimators=100, n_jobs=2)
+rf = RandomForestClassifier(n_estimators=100, oob_score=True, n_jobs=-1)
+rf.fit(train, target)
+prob = rf.predict_proba(test)
+savetxt(path.join(script_dir, 'data/submit.csv'),
+        prob, delimiter=',', fmt='%f')
+print rf.oob_score_
 
-    # create and train the random forest
-    # multi-core CPUs can use: rf =
-    # RandomForestClassifier(n_estimators=100, n_jobs=2)
-    rf = RandomForestClassifier(n_estimators=10, n_jobs=-1)
-    rf.fit(train, target)
-    savetxt(path.join(script_dir, 'data/submit.csv'), rf.predict_proba(test), delimiter=',', fmt='%f')
-
-
-if __name__ == "__main__":
-    main()
+# score = log_loss(target, prob)
+# print score
