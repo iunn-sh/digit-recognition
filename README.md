d97173d51499855b1e34de5dfae15490bb9efa22
jbytw <JBY@jbytw-mbp.client.tw.trendnet.org>
2016-04-08 11:41:00 +0800

    1. add .gitignore 2. add optimizer 3. refine recognizer

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # digit-recognition
-[![Circle CI](https://circleci.com/gh/jbytw/digit-recognition.svg?style=svg)](https://circleci.com/gh/jbytw/digit-recognition)
+[![Circle CI](https://circleci.com/gh/jbytw/digit-recognition.svg?style=shield)](https://circleci.com/gh/jbytw/digit-recognition)
 
 ## Commit History
-![out-of-bag accuracy](https://plot.ly/~jbytw/16/random-forest.png)
+[![out-of-bag accuracy](https://plot.ly/~jbytw/16/random-forest.png)](https://plot.ly/~jbytw/16/random-forest)
 
 ## Development Setup
 * python http://apple.stackexchange.com/questions/209572/how-to-use-pip-after-the-el-capitan-max-os-x-upgrade
@@ -13,10 +13,11 @@
 * `ref`     : collected sample code from the internet
 * `feature` : extracted feature set from `data`
 
-## File usage
-* `extractor.py`
-* `recognizer.py` : train, test, create .csv for submission
-* `plotter.py`  
+## File Usage
+* `extractor.py`  : pre-process, only run 1 time for a dataset
+* `recognizer.py` : train, test, create .csv for submission (main function)
+* `optimizer.py`  : calculate then plot out-of-bag accuracy for the range of trees (n_estimators)
+* `plotter.py`    : plot history accuracy to plotly, run at CircleCI
 
 ## Feature Extraction
 * color difference / slope
--- recognizer.py
+++ recognizer.py
@@ -1,9 +1,9 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import log_loss
-from numpy import genfromtxt, savetxt
+from numpy import genfromtxt, savetxt, std, argsort
 from os import path
 
-def random_forest():
+def create_forest():
     # create the training & test sets, skipping the header row with [1:]
     script_dir = path.dirname(__file__)  # <-- absolute dir the script is in
     dataset = genfromtxt(open(path.join(script_dir, 'data/train.csv'), 'r'),
@@ -16,16 +16,19 @@ def random_forest():
     # create and train the random forest
     # multi-core CPUs can use: rf =
     # RandomForestClassifier(n_estimators=100, n_jobs=2)
-    rf = RandomForestClassifier(n_estimators=100, oob_score=True, n_jobs=-1)
-    rf.fit(train, target)
-    prob = rf.predict_proba(test)
+    forest = RandomForestClassifier(n_estimators=100, oob_score=True, n_jobs=-1)
+    forest.fit(train, target)
+    prob = forest.predict_proba(test)
     savetxt(path.join(script_dir, 'data/sample.csv'),
             prob, delimiter=',', fmt='%f')
-    print "out-of-bag accuracy = ", rf.oob_score_
-    return rf.oob_score_
 
-    # score = log_loss(target, prob)
-    # print score
+    return forest
+
+def get_oob(forest):
+    print "out-of-bag accuracy = ", forest.oob_score_
+    return forest.oob_score_
+
 
 if __name__ == "__main__":
-    random_forest()
+    forest = create_forest()
+    get_oob(forest)
