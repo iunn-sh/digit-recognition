2663cdee8db06b4cf07a9ea920843ed42724eb4a
EC2 Default User <ec2-user@java-app1.localdomain>
2016-04-12 05:32:27 +0000

    add show data matrix

---
+++ showdata.py
@@ -0,0 +1,25 @@
+import sys
+import math
+import csv
+
+# python showdata.py filename data/train/
+
+
+PIXEL_COUNT = 28
+
+
+def show(fn, folder):
+    
+    with open(folder+fn, 'rb') as f:
+        px=f.read(PIXEL_COUNT)
+        while(len(px) > 0 ):
+            line = ''
+            for i in range(len(px)):
+                asci = ord(px[i])
+                line = '%s%s' %(line,str(asci).ljust(3))
+            print line 
+            px=f.read(PIXEL_COUNT)  
+            
+if __name__ == '__main__':
+    show(sys.argv[1], sys.argv[2])
+
