344ac9b2dd602a46e13928437160be6ef5d32bc0
chehao_hsiao <chehao.hsiao@gmail.com>
2016-04-14 17:55:37 +0800

    update simple model to multi layer nn deep learnning

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -94,6 +94,15 @@ def max_pool_2x2(x):
   	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                         strides=[1, 2, 2, 1], padding='SAME')
 
+def appendHeader(header,data):
+	out = []
+	for i in range(header):
+		row = []
+		row.extend(header[i])
+		row.extend(data[i])
+		out.append(row)
+	return out
+		
 mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
 ap_dir = path.dirname(__file__)  # <-- absolute dir the script is in
@@ -126,7 +135,15 @@ validation_labels = train_labels[:2000]
 print('validation_images({0[0]},{0[1]})'.format(validation_images.shape))
 print('validation_images({0[0]},{0[1]})'.format(validation_labels.shape))
 
-test_images = [[]]
+test_images = pd.read_csv(path.join(ap_dir, 'data/test.csv'),header=None)
+print('test_images({0[0]},{0[1]})'.format(test_images.shape))
+print (test_images.head())
+test_head = test_images.iloc[:,0:1].values
+test_images = test_images.iloc[:,1:].values
+
+# convert from [0:255] => [0.0:1.0]
+test_images = np.multiply(test_images, 1.0 / 255.0)
+print('test_images({0[0]},{0[1]})'.format(test_images.shape))
 test_labels = [[]]
 
 
@@ -139,22 +156,87 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 # Define loss and optimizer
+
+
+# multi layer model
+#layer 1
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+x_image = tf.reshape(x, [-1,28,28,1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+#layer 2
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+# densely
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+#dropout
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+#Readout
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
 y_ = tf.placeholder(tf.float32, [None, 10])
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 # Train
 tf.initialize_all_variables().run()
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+sess.run(tf.initialize_all_variables())
+for i in range(200):
+  batch = next_batch(train_images,train_labels,50)
+  if i%100 == 0:
+    train_accuracy = accuracy.eval(feed_dict={ x:batch[0], y_: batch[1], keep_prob: 1.0})
+    print("step %d, training accuracy %g"%(i, train_accuracy))
+  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+print("validation_images accuracy %g"%accuracy.eval(feed_dict={
+    x: validation_images, y_: validation_labels, keep_prob: 1.0}))
+
+"""    
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
 for i in range(1000):
   batch_xs, batch_ys = next_batch(train_images,train_labels,100)
   train_step.run({x: batch_xs, y_: batch_ys})
 
-# Test trained model
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
-print(accuracy.eval({x: validation_images, y_: validation_labels}))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))  
+"""
+#get prediction probability
 
+pred_prob = y_conv.eval(feed_dict={x: test_images, keep_prob: 1.0}, session=sess)
+print(test_head[1])
+out = np.concatenate((np.array(test_head), np.array(pred_prob)), axis=1)
+np.savetxt('data/submit-tf-nn.csv', out, delimiter=',', header = '')
+print(out[1])
+#out = np.concatenate((np.array(test_head).T, np.array(pred_prob)), axis=1)
+#print(out)
+#get prediction
+#prediction = tf.argmax(y,1)
+#print (prediction.eval(feed_dict={x: validation_images}))
 
-
+# Test trained model
