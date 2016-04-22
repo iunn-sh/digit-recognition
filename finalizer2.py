5fe4596922c58ba1452f5be57711c93eda58d319
chehao_hsiao <chehao.hsiao@gmail.com>
2016-04-22 22:48:13 +0800

    add finalizer2 ,and submit

---
+++ finalizer2.py
@@ -0,0 +1,32 @@
+'''python finalizer2.py input output''' 
+import sys
+import numpy as np
+import pandas as pd
+from os import path
+
+
+def finalizer(inputFile,outputFile):
+	data = pd.read_csv(path.join(".", inputFile),header=None)
+	header = data.iloc[:,0:1].values
+	data = data.iloc[:,1:].values
+	data = np.where(data > 0.99, 1 , data)
+	data = np.hstack((header, data))
+	np.savetxt(outputFile, data, delimiter=',', header = '', fmt='%s')
+
+def findNotSureData(inputFile,outputFile):
+	data = pd.read_csv(path.join(".", inputFile),header=None)
+	data = data.values
+	cond1 = (data[:,1]< 0.6) & (data[:,2]< 0.6) & (data[:,3]< 0.6) 
+	cond2 = (data[:,4]< 0.6) & (data[:,5]< 0.6) & (data[:,6]< 0.6)
+	cond3 = (data[:,7]< 0.6) & (data[:,8]< 0.6) & (data[:,9]< 0.6) & (data[:,10]< 0.6)
+	data = data[cond1&cond2&cond3]
+	np.savetxt(outputFile, data, delimiter=',', header = '', fmt='%s')       
+
+if __name__ == '__main__':
+    inputFile = sys.argv[1]
+    outputFile = sys.argv[2]
+    finalizer(inputFile,outputFile)
+    #print(len(sys.argv))
+    if(len(sys.argv) > 3):
+    	outputNotSure = sys.argv[3]
+    	findNotSureData(inputFile,outputNotSure)
